024553f492aecdfd0c337c325e11005550cbb925
48884 <tehix1@gmail.com>
2021-03-30 18:34:49 +0200

    prettier; add services files;

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,18 @@
-import "bulma/css/bulma.css";
-import React from "react";
+import 'bulma/css/bulma.css'
+import React from 'react'
 
-const Content = () =>{
-
-    return(
-        <div className={"container mt-5"}>
-            <div className={"columns is-mobile is-9" }>
-                <div className={"column is-3"}>
-                    <div className={"tile box"}>
-                        <p className={"title"}>
-                            test size
-                        </p>
+const Content = () => {
+    return (
+        <div className={'container mt-5'}>
+            <div className={'columns is-mobile is-9'}>
+                <div className={'column is-3'}>
+                    <div className={'tile box'}>
+                        <p className={'title'}>test size</p>
                     </div>
                 </div>
-                <div className={"column is-9"}>
-                    <div className={"tile box"}>
-                        <p className={"title"}>
-                            test second size
-                        </p>
+                <div className={'column is-9'}>
+                    <div className={'tile box'}>
+                        <p className={'title'}>test second size</p>
                     </div>
                 </div>
             </div>
