4bd7663c50bea56d835f729311d73d398abb90cc
48884 <tehix1@gmail.com>
2021-04-02 02:47:26 +0200

    fix menu on mobile

--- src/components/Top.js
+++ src/components/Top.js
@@ -5,6 +5,7 @@ import { searchName } from '../reducers/filtersReducer'
 
 const Top = () => {
     const [search, setSearch] = React.useState('')
+    const [active, setActive] = React.useState(false)
     const dispatch = useDispatch()
 
     const onChange = (e) => {
@@ -15,6 +16,10 @@ const Top = () => {
     const doSearch = (e) => {
         e.preventDefault()
         if (e.type === 'click' || e.which === '13') dispatch(searchName(search))
+    }
+
+    const toggleActive = () => {
+        setActive(!active)
     }
 
     return (
@@ -33,14 +38,26 @@ const Top = () => {
                             >
                                 COCKTAIL FINDER
                             </a>
+                            <a
+                                className={`navbar-burger ${
+                                    active ? 'is-active' : ''
+                                }`}
+                                role="button"
+                                onClick={toggleActive}
+                                data-target="navbarMenuHeroA"
+                            >
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </a>
                         </div>
-                        <span
-                            className="navbar-burger"
-                            data-target="navbarMenuHeroA"
+
+                        <div
+                            id="navbarMenuHeroA"
+                            className={`navbar-menu pb-0  pt-0 ${
+                                active ? 'is-active' : ''
+                            }`}
                         >
-                            <span></span>
-                        </span>
-                        <div id="navbarMenuHeroA" className="navbar-menu">
                             <div className="navbar-end">
                                 <a href="#" className="navbar-item is-active">
                                     Home
@@ -58,7 +75,7 @@ const Top = () => {
                     </div>
                 </nav>
             </div>
-            <div className={'hero-body'}>
+            <div className={'hero-body-padding-medium mt-6'}>
                 <div className={'container has-text-centered'}>
                     <span className="icon">
                         <i className="fas fa-cocktail icon is-large"></i>
@@ -83,7 +100,7 @@ const Top = () => {
                     </div>
 
                     <button
-                        className={'mt-5 pl-5 pr-5 button is-primary '}
+                        className={'mt-5 mb-5 pl-5 pr-5 button is-primary '}
                         onClick={doSearch}
                         onKeyDown={doSearch}
                     >
