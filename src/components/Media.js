337fe06a21f3d0a6127b697d584ee1697cf9aba1
48884 <tehix1@gmail.com>
2021-04-05 14:04:30 +0200

    update media ui

--- src/components/Media.js
+++ src/components/Media.js
@@ -4,34 +4,45 @@ import './Media.sass'
 
 const Media = ({ row }) => {
     return (
-        <div className="result">
-            <div className=" columns ">
-                <div className="column is-1">
-                    <figure className="image is-64x64">
-                        <img
-                            src={row.strDrinkThumb}
-                            className=""
-                            alt={`${row.strDrink} drink`}
-                        />
-                    </figure>
+        <article className="media result">
+            <figure className="media-left image is-128x128">
+                <img
+                    src={row.strDrinkThumb}
+                    className=""
+                    alt={`${row.strDrink} drink`}
+                />
+            </figure>
+            <div className={'column'}>
+                <div className={'columns'}>
+                    <span className="column has-text-left">
+                        <b>{row.strDrink}</b>
+                    </span>
+                    <span className="ml-3 has-text-right is-size-7">
+                        <span className="mr-2">{row.strAlcoholic}</span>
+                        <span className="mr-2">/</span>
+                        <span className="mr-2">{row.strCategory}</span>
+                        <span className="mr-2">/</span>
+                        <span className="mr-2">{row.strGlass}</span>
+                    </span>
                 </div>
-                <div className={'column'}>
-                    <div className={'columns'}>
-                        <span className="column has-text-left">
-                            <b>{row.strDrink}</b>
-                        </span>
-                        <span className="column has-text-right is-size-7">
-                            <span className="mr-2">{row.strAlcoholic}</span>
-                            <span className="mr-2">/</span>
-                            <span className="mr-2">{row.strCategory}</span>
-                            <span className="mr-2">/</span>
-                            <span className="mr-2">{row.strGlass}</span>
-                        </span>
-                    </div>
-                    <p>{row.strInstructions}</p>
-                </div>
+                <p>{row.strInstructions}</p>
+                <br/>
+                    <small>{[...Array(15).keys()]
+                        .map(key => ++key)
+                        .map((number) => {
+                            const ingredient = row[`strIngredient${number}`]
+                            const measure = row[`strMeasure${number}`]
+                            if (ingredient !== null)
+                                return(
+
+                                    <div className="mb-1"><b>{`${number}:`}</b> {`${ingredient}`} <i>{`${measure}`}</i></div>
+
+                                )
+                            return ''
+                        })}</small>
             </div>
-        </div>
+
+        </article>
     )
 }
 
